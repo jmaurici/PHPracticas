936ceac794a10abfdac670b942a0e97e65119945
JOSE MAURICIO SANCHEZ RUBIO <jmaurici@users.noreply.github.com>
2019-01-23 12:15:27 +0000

    Intro a PDO, prepared statement

---
+++ ejemplos/06_conectarBD_PDO.php
@@ -0,0 +1,49 @@
+<section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+    <div class="my-auto">
+        <h1 class="mb-0">Conexión a Base de datos con PDO
+
+        </h1>
+        <div class="subheading mb-5">
+            <?php
+
+            try {
+                $servername = "localhost";
+                $username = "root";
+                $password = "1234";
+                $database = "tienda";
+
+                $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $categoria = 1;
+                $sentencia = $conn->prepare("select * from books where category_id = :xyz");
+
+                $sentencia->bindParam(":xyz", $categoria);
+                $sql = "SELECT * FROM books where category_id =" . $categoria;
+
+                $sentencia->execute();
+                $results = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+                echo "<ul>";
+                foreach ($results as $k => $v) {
+                    echo "<li>";
+                    foreach ($v as $k1 => $v1)
+                        echo $k1 . " => " . $v1;
+                    echo "</li>";
+                }
+                echo "</ul>";
+            } catch (PDOException $ex) {
+                echo $ex->getMessage();
+            } catch (Exception $ex) {
+                echo $ex->getMessage();
+            } finally {
+                echo "Me ejecuto SIEMPRE";
+                $conn = null;
+            }
+            ?>
+        </div>
+    </div>
+</section>
+
+
+
